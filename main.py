2d497f1062342e38730e2c6c105b8acb1c09b378
Johnny-kiv <johnny.kiv@yandex.ru>
2022-11-03 17:41:34 +0300

    4

--- main.py
+++ main.py
@@ -26,7 +26,12 @@ async def bot_message(message: types.Message):
         await bot.send_message(message.from_user.id,"1.Стартрек Первый",reply_markup=nav.choiceMenu)
         await bot.send_message(message.from_user.id,"2.Стартрек Второй", reply_markup=nav.choiceMenu)
         await bot.send_message(message.from_user.id,"3.Стартрек Третий", reply_markup=nav.choiceMenu)
-        await bot.send_message(message.from_user.id,"4.Beliver Image Dragons", reply_markup=nav.choiceMenu)
+        await bot.send_message(message.from_user.id,"4.Belliver", reply_markup=nav.choiceMenu)
+        await bot.send_message(message.from_user.id,"5.Гарфилд Первый", reply_markup=nav.choiceMenu)
+        await bot.send_message(message.from_user.id,"6.Гарфилд Второй", reply_markup=nav.choiceMenu)
+        await bot.send_message(message.from_user.id,"7.Тор", reply_markup=nav.choiceMenu)
+        await bot.send_message(message.from_user.id,"8.Тор Царство Тьмы", reply_markup=nav.choiceMenu)
+        await bot.send_message(message.from_user.id,"9.Тор Рагнарёк", reply_markup=nav.choiceMenu)
     if message.text=="1":
         os.system("xdg-open /home/user/Видео/Zvezdnij.put.2009.XviD.DVDRip.ELEKTRI4KA.avi")
     if message.text=="2":
@@ -34,6 +39,16 @@ async def bot_message(message: types.Message):
     if message.text=="3":
         os.system("xdg-open /home/user/Видео/StarTrekBeyond.avi")
     if message.text=="4":
+        os.system("xdg-open /home/user/Видео/Beliver.mp4")
+    if message.text == "5":
+        os.system("xdg-open /home/user/Видео/Garfild.2004.XviD.DVDRip.Kinozal.TV.avi")
+    if message.text == "6":
+        os.system("xdg-open /home/user/Видео/Гарфилд 2. История двух кошечек.mkv")
+    if message.text == "7":
+        os.system("xdg-open /home/user/Видео//home/user/2011-Thor.avi")
+    if message.text == "8":
+        os.system("xdg-open /home/user/Видео/Beliver.mp4")
+    if message.text == "9":
         os.system("xdg-open /home/user/Видео/Beliver.mp4")
     if message.text=="Домой":
         await bot.send_message(message.from_user.id,"Главная",reply_markup=nav.mainMenu)
